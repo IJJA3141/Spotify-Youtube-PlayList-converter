46815c65e1d1ff258c58df7af2b99964241a50d0
IJJA3141 <alex.estella.cj@gmail.com>
2023-07-18 14:31:41 +0200

    00.00.00

--- backend/src/ipc/ipc-channel-interface.ts
+++
@@ -1,9 +0,0 @@
-import { IpcMainEvent } from 'electron'
-import IpcRequest from '../../../shared/ipc-request'
-
-interface IpcChannelInterface {
-  name:string,
-  handle(_envent: IpcMainEvent, _request: IpcRequest): void,
-}
-
-export default IpcChannelInterface
